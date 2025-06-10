44f729b229583f716a45a6437b2b5fe5e1e27a41
Marcin Sulecki <marcin.sulecki@sulmar.pl>
2025-06-10 14:50:44 +0200

    Dodanie singletona Lazy

--- src/01_CreationalsPatterns/SingletonPattern/ApplicationContext.cs
+++ src/01_CreationalsPatterns/SingletonPattern/ApplicationContext.cs
@@ -1,11 +1,23 @@
 ﻿using System;
+using System.Threading;
 
 namespace SingletonPattern;
 
 internal class ApplicationContext
 {
+    private static Lazy<ApplicationContext> _instance = new Lazy<ApplicationContext>(() => new ApplicationContext());
+    public static ApplicationContext Instance => _instance.Value;
+
     public string LoggedUser { get; set; }
     public DateTime LoggedOn { get; set; }
     public string Theme { get; set; }
+
+    public ApplicationContext()
+    {
+        LoggedOn = DateTime.Now;
+        Theme = "Dark";
+
+        Thread.Sleep(1000);
+    }
 
 }

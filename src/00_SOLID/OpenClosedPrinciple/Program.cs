ffc4e6144d52c3ffb2e62e708d5e9294ff7f2b54
Marcin Sulecki <marcin.sulecki@sulmar.pl>
2025-06-10 11:25:47 +0200

    Refactor: otwarte na rozszerzenie a zamknięci na modyfikację

--- src/00_SOLID/OpenClosedPrinciple/Program.cs
+++ src/00_SOLID/OpenClosedPrinciple/Program.cs
@@ -5,19 +5,40 @@
 // bez modyfikowania kodu źródłowego.
 
 
+var discountsA = new Dictionary<string, decimal>
+{
+    ["Regular"] = 0.9m,
+    ["Premium"] = 0.8m,
+    ["VIP"] = 0.5m,
+};
 
-DiscountCalculator calculator = new DiscountCalculator();
+var discountsB = new Dictionary<string, decimal>
+{
+    ["Regular"] = 0.7m,
+    ["Premium"] = 0.6m,
+    ["VIP"] = 0.4m,
+};
+
+DiscountCalculator calculator = new DiscountCalculator(discountsA);
 
 var discount = calculator.CalculateDiscount("Regular", 10);
 
+Console.WriteLine(discount);
 
-// Złe podejście – każda nowa zniżka wymaga modyfikacji:
+
+// Dobre podejście – otwarte na rozszerzenie a zamknięci na modyfikację ("działa? - nie tykaj!")
 public class DiscountCalculator
 {
+    private IDictionary<string, decimal> discounts;
+
+    public DiscountCalculator(IDictionary<string, decimal> discounts)
+    {
+        this.discounts = discounts;
+    }
+
     public decimal CalculateDiscount(string customerType, decimal total)
     {
-        if (customerType == "Regular") return total * 0.9m;
-        if (customerType == "Premium") return total * 0.8m;
-        return total;
+        return total * discounts[customerType];
     }
 }
+
